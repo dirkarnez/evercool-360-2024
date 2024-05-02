1d572338d7e70cc4a1c11b35af8aeec52f16e4c9
dirkarnez <smalldirkalex@gmail.com>
2024-05-02 21:23:49 +0800

    - upload files

--- periodic-commit.cmd
+++ periodic-commit.cmd
@@ -1,14 +1,15 @@
 set PATH=%USERPROFILE%\Downloads\PortableGit\bin
 
-:end
 cls &&^
 cd %cd% &&^
 C:\Windows\System32\timeout.exe /t 5 &&^
 echo committing &&^
+git config --global user.email "smalldirkalex@gmail.com" &&^
+git config --global user.name "dirkarnez" &&^
 git config --global credential.helper "" &&^
 git add * &&^
 git commit -m "- upload files" &&^
 git push -u origin main
-goto end
+
 
 pause
